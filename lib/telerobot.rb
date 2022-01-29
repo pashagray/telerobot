f00422ad43c49132087e8a00b772d7eecd2def69
Pavel Tkachenko <tpepost@gmail.com>
2022-01-29 12:23:44 +0600

    Fix bug that callback_query was not used

--- lib/telerobot.rb
+++ lib/telerobot.rb
@@ -33,11 +33,12 @@ module Telerobot
 
   def run(query, initial_state:, session_class:)
     symbolized_query = Utils.deep_symbolize_keys(query)
-    message = symbolized_query[:message]
-    chat = message[:chat]
+    callback_query = symbolized_query[:callback_query] || {}
+    message = symbolized_query[:message] || {}
+    chat = message[:chat] || callback_query[:message][:chat]
     session = session_class.find_or_create_by(chat_id: chat[:id])
     state_class = session.state ? session.state.constantize : initial_state
-    state_class.new.call(message, {}, session)
+    state_class.new.call(message, callback_query, session)
   end
 
   module_function :run
--- spec/telerobot_spec.rb
+++ spec/telerobot_spec.rb
@@ -23,5 +23,30 @@ RSpec.describe Telerobot do
         }.to output("start method invoked!").to_stdout
       end
     end
+
+    context "with callback_query" do
+      let(:params) do
+        {
+          "callback_query" => {
+            "data" => "/start",
+            "message" => {
+              "text" => "From inline keyboard",
+              "chat" => {
+                "id" => 1
+              }
+            }
+          }
+        }
+      end
+
+      let(:session_class) { Telerobot::SessionMock }
+      let(:initial_state) { StartState }
+
+      it "runs command for initial state" do
+        expect {
+          Telerobot.run(params, initial_state: initial_state, session_class: session_class)
+        }.to output("start method invoked!").to_stdout
+      end
+    end
   end
 end
